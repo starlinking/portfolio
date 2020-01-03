53de341bfaf293f5b57d414a4da5ee701537d97f
starlinking <star.idv@gmail.com>
2020-01-03 10:29:51 +0800

    first commint

---
+++ config.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+  url: 'https://starlinking.github.io/portfolio',
+  pathPrefix: '/portfolio',
+  title: 'Portfolio by Star',
+  subtitle: 'A journey of a thousand miles begins with a single step.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'techpubs',
+      path: '/'
+    },
+    {
+      label: 'about',
+      path: '/pages/about'
+    },
+    {
+      label: 'contact',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Star Chang',
+    photo: '/photo.jpg',
+    bio: 'A journey of a thousand miles begins with a single step.',
+    contacts: {
+      email: '',
+      facebook: '',
+      telegram: '',
+      twitter: '',
+      github: '#',
+      rss: '',
+      vkontakte: '',
+      linkedin: 'starlinking',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: ''
+    }
+  }
+};
